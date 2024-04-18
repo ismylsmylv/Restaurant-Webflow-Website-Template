1ac694b2ef1e24e4b9b6afd3b846ac31490c5d3b
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-04-18 20:48:24 +0400

    navbar updated

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,30 +1,102 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-
+import "./style.scss";
+import { FaCartShopping } from "react-icons/fa6";
+import Logo from "../../assets/img/Logo.png";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaPinterest } from "react-icons/fa";
 type Props = {};
 
 function Navbar({}: Props) {
   const navigate = useNavigate();
   return (
-    <div>
-      Navbar
-      <button
-        onClick={() => {
-          navigate("/about");
-        }}
-      >
-        about
-      </button>
-      <button
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        home
-      </button>
-      {/* <Link to={"/"}>home</Link>
-      <Link to={"/about"}>about</Link> */}
-    </div>
+    <>
+      <div className="navbar">
+        <div className="container">
+          <a href="tel:987 654 321" className="call">
+            Call - 987 654 321{" "}
+          </a>
+          <img className="logo" src={Logo} alt="" />
+          <div className="right">
+            <button className="shop">
+              <FaCartShopping size={25} color="white" />
+            </button>
+            <button className="reserve">Reservation</button>
+          </div>
+        </div>
+      </div>
+      <div className="navs">
+        <div className="container">
+          <div className="btns">
+            <button
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              home
+            </button>
+            <button
+              onClick={() => {
+                navigate("/about");
+              }}
+            >
+              about
+            </button>
+            <button
+              onClick={() => {
+                navigate("/menu");
+              }}
+            >
+              menu
+            </button>
+            <button
+              onClick={() => {
+                navigate("/reservation");
+              }}
+            >
+              reservation
+            </button>
+            <button
+              onClick={() => {
+                navigate("/gallery");
+              }}
+            >
+              gallery
+            </button>
+            <button
+              onClick={() => {
+                navigate("/blog");
+              }}
+            >
+              blog
+            </button>
+            <button
+              onClick={() => {
+                navigate("/contact");
+              }}
+            >
+              contact
+            </button>
+          </div>
+          <div className="socials">
+            <a className="socialBtn" href="#">
+              <FaInstagram size={25} color="white" />
+            </a>{" "}
+            <a className="socialBtn" href="#">
+              <FaFacebook size={25} color="white" />
+            </a>{" "}
+            <a className="socialBtn" href="#">
+              <FaTwitter size={25} color="white" />
+            </a>{" "}
+            <a className="socialBtn" href="#">
+              <FaPinterest size={25} color="white" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
